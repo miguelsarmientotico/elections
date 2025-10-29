601f921f232af1c9b63d0fdf2d176b388d66965a
miguelarch <sarmientomiguel3@gmail.com>
2025-10-28 20:04:52 -0500

    actualizacion de docker y script de actualizacion

--- test-em-all-docker.bash
+++ test-em-all-docker.bash
@@ -45,10 +45,52 @@ function assertEqual() {
   fi
 }
 
+function testUrl() {
+  url=$@
+  if $url -ks -f -o /dev/null
+  then
+    return 0
+  else
+    return 1
+  fi;
+}
+
+function waitForService() {
+  url=$@
+  echo -n "Wait for: $url... "
+  n=0
+  until testUrl $url
+  do
+    n=$((n + 1))
+    if [[ $n == 100 ]]
+    then
+      echo " Give up"
+      exit 1
+    else
+      sleep 3
+      echo -n ", retry #$n "
+    fi
+  done
+  echo "DONE, continues..."
+}
+
 set -e
+
+echo "Start Tests:" `date`
 
 echo "HOST=${HOST}"
 echo "PORT=${PORT}"
+
+if [[ $@ == *"start"* ]]
+then
+  echo "Restarting the test environment..."
+  echo "$ docker compose down --remove-orphans"
+  docker compose down --remove-orphans
+  echo "$ docker compose up -d"
+  docker compose up -d
+fi
+
+waitForService curl http://$HOST:$PORT/candidate-composite/$CAND_ID_CMTS_NEWS
 
 assertCurl 200 "curl http://$HOST:$PORT/candidate-composite/$CAND_ID_CMTS_NEWS -s"
 assertEqual $CAND_ID_CMTS_NEWS $(echo $RESPONSE | jq .candidateId)
@@ -63,5 +105,12 @@ assertEqual "\"Invalid candidateId: -1\"" "$(echo $RESPONSE | jq .message)"
 
 assertCurl 400 "curl http://$HOST:$PORT/candidate-composite/invalidProductId -s"
 assertEqual "\"Type mismatch.\"" "$(echo $RESPONSE | jq .message)"
+
+if [[ $@ == *"stop"* ]]
+then
+    echo "We are done, stopping the test environment..."
+    echo "$ docker compose down"
+    docker compose down
+fi
 
 echo "End, all tests OK:" `date`
